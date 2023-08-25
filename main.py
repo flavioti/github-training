f051b6aa2ad169d36cd406eed4cdea7901a18b66
Flavio Sousa <flaviomarcioti@gmail.com>
2023-08-25 19:17:36 -0300

    Primeiro commit pelo terminal

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello World")
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+
+driver.get("https://www.deriv.com")
+
+text_box = driver.find_element(by=By.NAME, value="my-text")
+submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
+
+print(driver)
